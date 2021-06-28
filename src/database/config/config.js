b590754297e578e8b37b8cb5b3c31a1db1300a66
FedericoMelgarejo <66423135+FedericoMelgarejo@users.noreply.github.com>
2021-06-28 13:37:27 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,24 +1,24 @@
 module.exports = {
-
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "universo_camping",
-    "host": "127.0.0.1",
+    "username": "uyxijb2xc2lzwunb",
+    "password": "hfMs3UNt53rc7bDGxBRx",
+    "database": "bkmscs9lzldrksfa76qd",
+    "host": "bkmscs9lzldrksfa76qd-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "uyxijb2xc2lzwunb",
+    "password": "hfMs3UNt53rc7bDGxBRx",
+    "database": "bkmscs9lzldrksfa76qd",
+    "host": "bkmscs9lzldrksfa76qd-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "uyxijb2xc2lzwunb",
+    "password": "hfMs3UNt53rc7bDGxBRx",
+    "database": "bkmscs9lzldrksfa76qd",
+    "host": "bkmscs9lzldrksfa76qd-mysql.services.clever-cloud.com",
     "dialect": "mysql"
   }
 }
+
