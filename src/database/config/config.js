ffca12ea5cf41a7265a270e0c6b13fd5f6c120ce
FedericoMelgarejo <66423135+FedericoMelgarejo@users.noreply.github.com>
2021-06-29 20:16:50 -0300

    Update config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,4 +1,5 @@
-module.exports = {
+//db clever cloud
+/*module.exports = {
   "development": {
     "username": "uyxijb2xc2lzwunb",
     "password": "hfMs3UNt53rc7bDGxBRx",
@@ -18,6 +19,30 @@ module.exports = {
     "password": "hfMs3UNt53rc7bDGxBRx",
     "database": "bkmscs9lzldrksfa76qd",
     "host": "bkmscs9lzldrksfa76qd-mysql.services.clever-cloud.com",
+    "dialect": "mysql"
+  }
+}*/
+//db db4free
+module.exports = {
+  "development": {
+    "username": "uni_camping",
+    "password": "123456123456",
+    "database": "universo_camping",
+    "host": "db4free.net",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "uni_camping",
+    "password": "123456123456",
+    "database": "universo_camping",
+    "host": "db4free.net",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "uni_camping",
+    "password": "123456123456",
+    "database": "universo_camping",
+    "host": "db4free.net",
     "dialect": "mysql"
   }
 }
